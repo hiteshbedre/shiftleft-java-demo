757dd48c1d0b4857c26b7a1141f6cb22253c841f
hiteshbedre <32206192+hiteshbedre@users.noreply.github.com>
2025-01-06 21:45:32 +0530

    Update users.sql

--- users.sql
+++ users.sql
@@ -23,6 +23,9 @@ DELETE FROM users WHERE username = 'jane_doe';
 -- Delete a user from the 'users' table
 DELETE FROM users WHERE username = 'jane_doe';
 
+-- Delete a user from the 'users' table
+DELETE FROM users WHERE username = 'jane_doe';
+
 
 
 
